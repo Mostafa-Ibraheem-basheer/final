f68a0081a1f9997f177678c68d21cf76752c17d7
Mostafa-Ibraheem-basheer <Mostafa.basheer00@eng-st.cu.edu.eg>
2022-10-31 15:33:24 +0200

    update

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://khadajhin.eba-9gd64efm.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
