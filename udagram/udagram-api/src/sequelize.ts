b4a51ac2c6d3afe104763a562bf8d751f181eab3
Mostafa-Ibraheem-basheer <Mostafa.basheer00@eng-st.cu.edu.eg>
2022-11-06 22:53:36 +0200

    update

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,12 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-
-  dialect: "postgres",
-  storage: ":memory:",
-});
+export const sequelize = new Sequelize(
+  "postgres://postgres:atabase-1.caw1ygi7mnu0.us-east-1.rds.amazonaws.com:5432/postgres"
+);
