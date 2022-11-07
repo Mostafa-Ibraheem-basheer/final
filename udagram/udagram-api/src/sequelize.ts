5a693c1ea219711f5e45bb8cf75490cb411b0cbe
Mostafa-Ibraheem-basheer <Mostafa.basheer00@eng-st.cu.edu.eg>
2022-11-07 22:30:57 +0200

    sadsadsa

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -2,5 +2,5 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize(
-  "postgres://postgres:database-1.caw1ygi7mnu0.us-east-1.rds.amazonaws.com:5432/postgres"
+  "postgres://postgres:postgres@database.caw1ygi7mnu0.us-east-1.rds.amazonaws.com:5432/postgres"
 );
