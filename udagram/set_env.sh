699333665bfd2de5a2edabf55ed3615143cd888d
Mostafa-Ibraheem-basheer <Mostafa.basheer00@eng-st.cu.edu.eg>
2022-11-08 11:46:06 +0200

    dsada

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,12 +2,12 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 POSTGRES_USERNAME=postgres
 POSTGRES_PASSWORD=postgres
-POSTGRES_HOST=database-1.caw1ygi7mnu0.us-east-1.rds.amazonaws.com
+POSTGRES_HOST=database.caw1ygi7mnu0.us-east-1.rds.amazonaws.com
 POSTGRES_DB=postgres
 AWS_BUCKET=arn:aws:s3:::khadajhin
 AWS_REGION=us-east-1
 AWS_PROFILE=default
 JWT_SECRET=mysecretstring
-URL=http://khadajhin-env.eba-c29rf39a.us-east-1.elasticbeanstalk.com/
+URL=http://udagram-api-env.eba-753c9txn.us-east-1.elasticbeanstalk.com
 AWS_ACCESS_KEY_ID=AKIA2ESYRUP4WZRL22DA
 AWS_SECRET_ACCESS_KEY=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
